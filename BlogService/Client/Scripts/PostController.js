0c4c230bd8ab7fbbc6e61c3629ebd2334d3f58f0
mahikero <federicoparassarmiento@yahoo.com>
2016-09-09 12:30:34 -0400

    up

---
+++ BlogService/Client/Scripts/PostController.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    var PostController = function ($scope, $http) {
+        $http.get("/api/posts")
+             .success(function (data) {
+                 $scope.posts = data;
+             });
+    };
+
+    app.controller("PostController", PostController);
+}(angular.module("blogInfo")));
