96935f4638daa8eaed151ac678450cc6fbda4563
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2016-09-11 04:02:51 +0800

    up

--- BlogService/Client/Scripts/PostController.js
+++ BlogService/Client/Scripts/PostController.js
@@ -5,6 +5,22 @@
             .success(function (data) {
                 $scope.posts = data;
             });
+
+        $scope.delete = function (post) {
+            postService.delete(post)
+                .success(function () {
+                    removePostById(post.Id);
+                });
+        };
+
+        var removePostById = function (id) {
+            for (var i = 0; i < $scope.posts.length; i++) {
+                if ($scope.posts[i].Id == id){
+                    $scope.posts.splice(i, 1);
+                    break;
+                }
+            }
+        };
     };
 
     app.controller("PostController", PostController);
--- BlogService/Client/Scripts/postService.js
+++ BlogService/Client/Scripts/postService.js
@@ -8,9 +8,24 @@
             return $http.get(postsApiUrl + id);
         };
 
+        var update = function (post) {
+            return $http.put(postsApiUrl + post.Id, post);
+        };
+
+        var create = function (post) {
+            return $http.post(postsApiUrl, post);
+        };
+
+        var destroy = function (post) {
+            return $http.delete(postsApiUrl + post.Id);
+        };
+
         return {
             getAll: getAll,
-            getById: getById
+            getById: getById,
+            update: update,
+            create: create,
+            delete: destroy
         };
     };
 
